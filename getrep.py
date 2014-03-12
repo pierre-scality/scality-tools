849795e29d6ae5d4a44684560a33027b65622eb3
root <root@sup.(none)>
2014-03-12 14:49:49 +0000

    Add -r to random create keys and add sorting for output (COS related)

--- getrep.py
+++ getrep.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python2
 
 import sys
+import os
 import time
+import getopt
  
 sys.path.insert(0,'/usr/local/scality-ringsh/ringsh/modules')
 from scality.supervisor import Supervisor
 from scality.node import Node
 from scality.key import Key
+from scality.key import KeyRandom
 
+PRGNAME=os.path.basename(sys.argv[0])
 LENKEY=40
+COS='20'
+keylist=[]
 
 def usage():
 	message="usage : "+PRGNAME
 	add="""
 	KEY
+	or 
+	-r (random key number)
 """
 	print(message+add)
 
-def parseargs(argv,option):
+def parseargs(argv):
 	if len(argv)==0:
-		return option
+		usage()
+		sys.exit(1)
+	if len(argv)==1:
+		k=sys.argv[1] 
 	try:
-		opts, args = getopt.getopt(argv, "AdF:hLoPs:t:T:qvz:", ["help"])
+		opts, args = getopt.getopt(argv, "r:", ["help"])
 	except getopt.GetoptError:
-		Message.fatal(PRGNAME,"Argument error",10)
+		print "Argument error"
+		usage()
+		sys.exit(0)
 	for i,el in enumerate(opts):
 		if '-d' in el:
 			opts.pop(i)
@@ -31,8 +44,15 @@ def parseargs(argv,option):
 		if opt in ("-h", "--help"):
 			usage()
 			end(0)
-		elif opt == '-A':
-			option['ALL']=1
+		elif opt == '-r':
+			for i in range(int(arg)):
+				k=KeyRandom(COS).getHexPadded()
+				keylist.append(k)
+				
+	if len(args) > 0:
+		for i in args:
+                        keylist.append(i)
+	return keylist 
 
 def printreplica(key):
 	local=[]
@@ -41,9 +61,9 @@ def printreplica(key):
 	return local
 
 #print sys.argv[1:]
-list=sys.argv[1:]
+parseargs(sys.argv[1:])
 rez=[]
-for i in list :
+for i in keylist :
 	#print 'debut %s' % i
 	try: 
 		k=Key(i) 
@@ -55,6 +75,7 @@ for i in list :
 		continue
 	rez=printreplica(k)
 	rez.append(i)
+	rez=sorted(rez,key=lambda d: d[38:])
 	for j in rez:
 		print j,
 	print
