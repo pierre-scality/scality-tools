de6fb5a2c537f4044ba2382a253618bc64e74e93
root <root@sup.(none)>
2014-04-04 14:11:04 +0000

    Change parameters and loop file

--- getrep.py
+++ getrep.py
@@ -10,19 +10,24 @@ from scality.supervisor import Supervisor
 from scality.node import Node
 from scality.key import Key
 from scality.key import KeyRandom
+from scality.daemon import DaemonFactory , ScalFactoryExceptionTypeNotFound
 
 PRGNAME=os.path.basename(sys.argv[0])
 LENKEY=40
 COS='20'
 keylist=[]
 option={}
+sup ='http://127.0.0.1:5580'
+ring='ring1'
+dummy=[]
+file=None
 
 def usage():
 	message="usage : "+PRGNAME
 	add="""
 	KEY
 	or
-	-r (print replica along with key) 
+	-s (print replica along with key) 
 	-x NuMBER (random key number)
 """
 	print(message+add)
@@ -34,61 +39,99 @@ def parseargs(argv):
 	if len(argv)==1:
 		k=sys.argv[1] 
 	try:
-		opts, args = getopt.getopt(argv, "x:", ["help"])
+		opts, args = getopt.getopt(argv, "Af:sr:x:", ["help"])
 	except getopt.GetoptError:
 		print "Argument error"
 		usage()
-		sys.exit(0)
+		sys.exit(9)
 	for i,el in enumerate(opts):
 		if '-d' in el:
 			opts.pop(i)
+	
 	for opt, arg in opts:
+		dummy.append(opt)
 		if opt in ("-h", "--help"):
 			usage()
 			end(0)
+		elif opt == '-A':
+			option.update(auto=1)
 		elif opt == '-x':
 			for i in range(int(arg)):
 				k=KeyRandom(COS).getHexPadded()
 				keylist.append(k)
+		elif opt == '-r':
+			ring=arg
 		elif opt == '-s':
-			option.update(successor='yes')
-				
+			option.update(successor='yes')	
+		elif opt == '-f':
+			file=arg 
+			try:
+                    		fp = open(arg,"r")
+            		except IOError as e :
+                        	print e
+				print 'impossible to open %s for read' % file
+				exit(9)	
+			fp.close()
+			option.update(file=arg)			
 	if len(args) > 0:
 		for i in args:
                         keylist.append(i)
 	return keylist 
 
-def printreplica(key):
+def printreplica(k):
 	local=[]
 	for i in k.getReplicas():
 		local.append(i.getHexPadded())
 	return local
 
-def node_init(ip,ring):
-	ringstat=s.supervisorConfigDso(dsoname=ring)
-	for n in ringstat['nodes']:
-        	nid = '%s:%s' % (n['ip'], n['chordport'])
-        	nodes[nid] = DaemonFactory().get_daemon("node", url='https://{0}:{1}'.format(n['ip'], n['adminport']), chord_addr=n['ip'], chord_port=n['chordport'], dso=ring)
-        if not node: node = nodes[nid]
-	return nodes
-
-
 
 parseargs(sys.argv[1:])
+nodes={}
+nodestatus={}
+names={}
 rez=[]
-for i in keylist :
-	#print 'debut %s' % i
+node=""
+s = Supervisor(url=sup)
+ringstat=s.supervisorConfigDso(action="view", dsoname=ring)
+for n in ringstat['nodes']:
+	nid = '%s:%s' % (n['ip'], n['chordport'])
+	nodes[nid] = DaemonFactory().get_daemon("node", url='https://{0}:{1}'.format(n['ip'], n['adminport']), chord_addr=n['ip'], chord_port=n['chordport'], dso=ring)
+	names[nid]=n['name']
+	nodestatus[nid]=nodes[nid].nodeGetStatus()[0]
+	if not node: node = nodes[nid]
+
+def main(key):
 	try: 
-		k=Key(i) 
+		k=Key(key) 
 	except ValueError:
-		print "%s is not a valid Key" % i
-		continue
-	if len(i) != LENKEY :
-		print "%s is not a valid Key length" % i
-		continue
+		print "%s is not a valid Key" % key
+		return()
+	if len(key) != LENKEY :
+		print "%s is not a valid Key length" % key
+		return()
 	rez=printreplica(k)
-	rez.append(i)
-	rez=sorted(rez,key=lambda d: d[LENKEY-2:])
-	for j in rez:
-		print j,
+	rez.append(key)
+	#rez=sorted(rez,key=lambda d: d[LENKEY-2:])
+	for K in rez:
+		if 'successor' in option:
+			suc=node.findSuccessor(K)['address']
+			name=names[suc]
+			print K,name
+		else:
+			print K
 	print
+
+if 'file' in option:
+	file=option['file']
+
+if file == None:
+	for key in keylist :
+		main(key)
+else:
+	with open(file, 'r') as fd:
+		for key in fd:
+			key=key.strip()
+			#print key,len(key)
+			if len(key) == LENKEY:
+				main(key)
+	fd.close()
