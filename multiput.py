af36ed1f4cc324278357812c538315c50c3dd533
manifestonosuke <root@sup.(none)>
2014-10-05 10:17:00 +0000

    multiput v0.9

--- multiput.py
+++ multiput.py
@@ -11,15 +11,20 @@ import sys
 import os
 import getopt 
 import subprocess
+import requests
+from time import *
 
 PRGNAME=os.path.basename(sys.argv[0])
 #print PRGNAME
 
-SLEEP=0.1
+SLEEP=2
 URI="cdmi1/blob2/"
 OP="PUT"
-
+HTTPTIMEOUT=3
 URL='http://'+URI
+RRRRR="."
+HEADERS=0
+
 
 
 def __print(level,prgname,message,option=""): 
@@ -37,7 +42,7 @@ def parseargs(argv):
 	global URL
 	#print "Parsing args",argv,"loop"
 	try:
-		opts, args = getopt.getopt(argv, "hu:d", ["help", "url="])
+		opts, args = getopt.getopt(argv, "dhHp:u:", ["help", "url="])
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
@@ -49,53 +54,142 @@ def parseargs(argv):
 		elif opt == '-d':
 			global DEBUG
 			DEBUG = 1 
+		elif opt == '-H':
+			global HEADERS
+			HEADERS = 1 
 		elif opt in ("-u", "--url"):
-			print "using URL",URL
 			URL = arg
-			remain = "".join(args)
-			print "remainingargs",remain
-			URL=remain
-	print "URL",URL
+			print "using URL",URL
+			#remain = "".join(args)
+			#print "remainingargs",remain
+			#URL=remain
+		elif opt in ("-p", "--path"):
+			global RRRRR
+			RRRRR = arg
+			print "using PATH ",RRRRR
 
-if (len(sys.argv) == 1) : 
-	print "Using default URL %s" % URL
-else:
+def format_filename(fullname):
+	""" return formated directory as list [ dir, file ] dir is stripped from ./"""
+	if fullname[:2] == './' : fullname=fullname[2:]	
+	if '/' not in fullname:
+		rez=["",fullname]
+	else:
+		(dir,file)=os.path.split(fullname)
+		rez=[dir,file]
+	return(rez)
+	
+
+def create_container(root,path,recurse=1):
+	""" create container can recurse down"""
+	url=root+"/"+path
+	response=requests.put(url)
+	if response.status_code == 201:
+		print "URL created "+url
+	else:
+		relative=""
+		for i in path.split('/'):
+			relative=relative+i+"/"
+			print "Creating "+URL+relative	
+			response=requests.put(URL+relative)
+		if response.status_code != 201:
+			print "Failed to create "+url
+			return response.status_code
+	return response.status_code	
+
+
+def put_file(url,datafile="none",option=""):
+	""" put file if container do not exist call create_conainer"""
+	#datafile="/root/scality-tools/"+datafile
+	#print "put "+url+","+datafile
+	#try:
+	with open(datafile,'rb') as fd:
+    		response=requests.put(url,data=fd)
+		#print str(response.status_code)+","+str(response.headers)
+		#print response.status_code
+	#except:
+	#	print "Error openning file "+datafile
+	#	return "Error"
+	if response.status_code == 404 : 
+		dir=format_filename(datafile)[0]
+		print "Creating intermediate containers "+dir
+		rez=create_container(URL,dir)
+		if rez != 201 :
+			print "FAILED to create file"+datafile
+			return 500 	
+		with open(datafile,'rb') as fd:
+    			response=requests.put(url,data=fd)
+			if str(response.status_code)[0] != 2 :
+				print "FAILED to create file"+datafile
+				return 500 
+	#else:
+	#	print "AERAZERAZER"	
+				
+	if HEADERS == 1:
+		for i in response.headers:
+			print i+" "+response.headers[i]
+		print "End Headers"
+
+		
+	return str(response.status_code)
+		
+
+if (len(sys.argv) != 1) : 
+#	print "Using default URL %s" % URL
+#else:
 	parseargs(sys.argv[1:])
 
-#print "req url",URL
-#req=urllib2.Request(URL) 
-
-#print "full",req.get_full_url()
-#print "method",req.get_method()
-
 try:
-	page=urllib2.urlopen(URL)
-except urllib2.HTTPError as e:
+	page=requests.get(URL,timeout=HTTPTIMEOUT)
+except requests.HTTPError as e:
 	print "code",e.code
 	print "read",e.read() 
 	print "len",len(e.read())
+except requests.Timeout	 as e:
+	print "Timeout processing page "+URL
+except requests.exceptions.ConnectionError: 
+	print "Cannot connect to "+URL
+#except :
+#	print page.status_code
+#	print "error processing page URL"
 else:
-	code=page.getcode()
-	print "return code "+str(code)
+	code=page.status_code
 	if str(code)[0] != "2" :
 		print "page not accessible "+str(code)
+		exit(1)
 
-cmd="/usr/bin/find"
-cmd="/usr/bin/find . -type f" 
+def main():
+	print "loaded"
+	return
+
+if __name__ != "__main__":
+    sys.exit(main())
+
+
+
+cmd="/usr/bin/find "+RRRRR+" -type f" 
+print RRRRR+" "+URL
+cmd=cmd.split()
 #(output, err) = p.communicate()
 count=0
 ps=subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 # shell=True)
 while True:
-	if ps.poll() != None:
-		break
 	try:
-		# fsarchiver do write on stderr
 		nextline = next(ps.stdout)
 	except StopIteration:
+		print "End of list"
 		break
 	count+=1
 	line=nextline.decode('utf-8').rstrip('\n')
-	print str(count)+" "+line
-
-
+	if line[:2] == './' : line=line[2:]
+	this_line=format_filename(line)
+	if this_line[1] == "" :
+		print "Putting container"+line
+		create_container(this_line[0])
+	else:
+		print "Putting file"+line
+		if URL[-1:] != "/" : URL=URL+"/"	
+		rez=put_file(URL+line,line)
+		print str(count)+" "+str(rez)+" "+line
+	print
+	sleep(SLEEP)
