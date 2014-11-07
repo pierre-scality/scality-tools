5a0dcf25fdc028acc866cf1f632cdc17e3ebee05
manifestonosuke <pierrenosuke@gmail.com>
2014-11-07 18:39:27 +0100

    can use ringsh files (file must be named ringsh.txt)

--- kscount.py
+++ kscount.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python2
-
-""" count the number of keys per range of keyspace """
-""" kscount.py <file> where file is result of supervisor ringstatus"""
-
 import os 
 import sys 
 
@@ -11,21 +7,39 @@ f=open(file)
 keyspace=[]
 node={}
 FF='F'*40
+
+"""
+ringStatus 
+Node: node1-n4 192.177.1.11:8087 0000000000000000000000000000000000000000 RUN
+ringsh.tx
+supervisor nodeAssignId RING Server10 8085 CE38E38EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA40
+
+"""
+
 while True:
 	l=f.readline()
-	if not l:
+        if not l:
 		#print 'end'
 		break
 	line=l.split()
-        if line[0] == "Node:" :
-            node[line[3]]=line[1]
-            key=line[3]
-            #print key
-            keyspace=keyspace+[key]
-            #print keyspace
+	#print line
+        if file=="ringsh.txt":
+            node[line[5]]=line[3]
+            key=line[5]
+            #print 'ringsh '+key
+        else:
+            #print 'node '+str(line)
+            #if line[0] == "Node:" :
+            node[line[2]]=line[0].split('-')[0]
+            key=line[2]
+            #print 'ringstatus '+key
+        keyspace=keyspace+[key]
+        #print keyspace
 keyspace.sort()
+#print node
 #print len(keyspace)
 #exit()
+serverkey={}
 for i in range(len(keyspace)):
     if i == len(keyspace)-1:
         succ=keyspace[0]
@@ -33,5 +47,12 @@ for i in range(len(keyspace)):
     else:
         succ=keyspace[i+1]
         nb=int(succ,16)-int(keyspace[i],16)
-
+    if node[keyspace[i]] not in serverkey.keys():
+        serverkey[node[keyspace[i]]]=nb
+    else:
+        serverkey[node[keyspace[i]]]=serverkey[node[keyspace[i]]]+nb
     print node[keyspace[i]],"\t",keyspace[i],succ,nb
+
+print "server total keys count"
+for server in serverkey.keys():
+    print server,"\t",serverkey[server]
