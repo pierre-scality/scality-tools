49e105f4f1927f2d1995af484fd871685a7f108d
Pierre <pierre.merle@scality.com>
2017-11-08 16:46:41 +0900

    add -t and -l task options

--- ring_task.py
+++ ring_task.py
@@ -3,6 +3,7 @@
 import sys
 import logging 
 import time
+import getopt
 
 sys.path.append('/usr/local/scality-ringsh/ringsh/modules')
 sys.path.append('/usr/local/scality-ringsh/ringsh')
@@ -17,12 +18,52 @@ p="DvEbRF8REAgz"
 
 logging.basicConfig(format='%(levelname)s : %(funcName)s: %(message)s',level=logging.INFO)
 logger = logging.getLogger()
+PRGNAME=sys.argv[0]
 
-#logger.setLevel(logging.DEBUG)
+def usage():
+        message="""
+        Check running tasks (default move) and display some stats
+        default time is 60s
+
+
+        usage : """+PRGNAME
+        add="""
+	-l list of task to display tasks (default move), list list move,rebuild ..
+	-r ring on which run the check 
+        -t interval between checks
+"""
+        print(message+add)
+
+def parseargs(argv):
+        option={}
+        try:
+                opts, args = getopt.getopt(argv, "dl:r:t:", ["help"])
+        except getopt.GetoptError:
+                print "Argument error"
+                usage()
+                sys.exit(9)
+        for i,el in enumerate(opts):
+                if '-d' in el:
+                        opts.pop(i)
+                        logger.setLevel(logging.DEBUG)
+        for opt, arg in opts:
+                if opt in ("-h", "--help"):
+                        usage()
+                        end(0)
+                elif opt == '-l':
+			dummy=arg.split(',')
+                        option.update(display=dummy)
+                elif opt == '-t':
+                        option.update(timer=arg)
+                elif opt == '-r':
+                        option.update(ring=arg)
+        #if len(args) > 0:
+        #       for i in args:
+	return option
 
 class ring_obj():
   """ class to manipulate ring objects"""
-  def __init__(self,ring="DATA",url="https://127.0.0.1:2443",l=None,p=None,timer=10):
+  def __init__(self,option,ring="DATA",url="https://127.0.0.1:2443",l=None,p=None):
     import config as ringsh_config
     self.url=url
     if l == None:
@@ -33,13 +74,20 @@ class ring_obj():
       self.p=ringsh_config.default_config['auth']['password']
     else:
       self.p=p
+    if "timer" in option.keys():
+      self.timer=int(option['timer'])
+    else:
+      self.timer=10
     self.ring=ring
     self.sup = Supervisor(url=self.url, login=self.l, passwd=self.p)
     self.tasks={}
-    self.timer=timer
+    if "display" in option.keys():
+      self.display=option['display']
+    else:
+      self.display=['move']
     self.prev={}
     self.current={}
-    
+ 
   def getconf(self):
     try:
       self.config=self.sup.supervisorConfigDso(action="view",dsoname=self.ring)
@@ -53,12 +101,12 @@ class ring_obj():
   def get_task_list(self,type="all"):
     dict={}
     try:
-      self.tasks=self.sup.supervisorTasksDso(dsoname="OWRING")
+      self.tasks=self.sup.supervisorTasksDso(dsoname=self.ring)
     except Exception as e:
       logger.info("Error getting tasks")
       print e
-    #tasks=self.sup.supervisorTasksDso(ringname)
     for task in self.tasks["tasks"]:
+      logger.debug("Initial Task {0}".format(task))
       if task['type'] not in dict.keys():
         dict[task['type']]=[]
       dict[task['type']].append(task)
@@ -71,20 +119,27 @@ class ring_obj():
     for i in self.task.keys():
       if type == "all" or i == type:
         logger.debug('Displaying task type {0}'.format(i))
-        for j in self.task[i]:
-          logger.debug('Displaying task type {0}'.format(str(j.keys)))
+        #for j in self.task[i]:
+        #  logger.debug('Displaying task type detail {0}'.format(str(j.keys)))
     return(0)
 
-  def print_task_stat(self,type="all"): 
-    logger.debug("Enter print_task_stat")
+  def print_task_filter(self,type="all"): 
+    logger.debug("Enter print_task_filter {0}".format(self.display))
+    if all in self.display:
+      self.display=['all']
+    done=0
     for task in self.tasks['tasks']:
       cur=task['type']
-      if cur == 'move' or cur == 'rebuild':
-        self.print_task_stat_move(task,cur)
-    return(0)
+      if cur in self.display or self.display[0]=="all":
+      #if cur == 'move' or cur == 'rebuild':
+        self.print_task_stat_chosen(task,cur)
+	done+=1
+    if done == 0:
+	print "No task to display ({0})".format(','.join(self.display))
+    return(done)
 
-  def print_task_stat_move(self,task,type):
-    logger.debug("Enter print_task_stat_move {0}".format(str(task)))
+  def print_task_stat_chosen(self,task,type):
+    logger.debug("Enter print_task_stat_chosen {0}".format(str(task)))
     node=task['node']['name']
     total=int(task['total'])
     current=int(task['done'])
@@ -111,12 +166,13 @@ class ring_obj():
     print
 
 def main():
-  o=ring_obj(ring="OWRING",timer=5)
+  option=parseargs(sys.argv[1:])
+  o=ring_obj(option,ring="OWRING")
   o.getconf()
   while True:
     o.get_task_list()
     o.print_task()
-    o.print_task_stat()
+    o.print_task_filter()
     o.wait_iter()  
   sys.exit(0)
 
