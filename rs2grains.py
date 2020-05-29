73012a04063b338fb118c1c60f1d12f01cd7312c
pierre <pierre@scality.com>
2020-05-29 16:53:15 +0900

    Add mode for primary iface

--- rs2grains.py
+++ rs2grains.py
@@ -5,6 +5,7 @@ import os
 import sys
 from datetime import datetime
 import argparse
+import re
 import salt.client
 import salt.config
 
@@ -13,6 +14,7 @@ parser.add_argument('-d', '--debug', dest='debug', action="store_true", default=
 parser.add_argument('-v', '--verbose', dest='verbose', action="store_true", default=False ,help='Set script in VERBOSE mode ')
 parser.add_argument('-t', '--target', nargs=1, const=None, required=True, help='MANDATORY Specify target host')
 parser.add_argument('-c', '--count', nargs=1, const=None, required=True, help='MANDATORY Interface count for RS2 connector')
+parser.add_argument('-p', '--primary', nargs=1, const=None, required=True, help='Do not use secondary but primary in order starting from the given iface')
 parser.add_argument('-s', '--set', dest='set', action="store_true", default=False, help='If this argument is used it will set the grains RS2IF_X with the order values of secondary iface from scality:prod_iface')
 
 
@@ -104,6 +106,38 @@ def get_secondary_sorted(dict,iface):
     list.append(temp[i])
   return(list)    
 
+def get_primary_sorted(dict,iface):
+  if not iface in dict.keys():
+    display.error("No interface {1} found in {0}".format(dict.keys(),iface))
+    exit(9)
+  pref=re.split('(\d+)',iface)[0]
+  display.debug("using if prefix {0}".format(pref))
+  temp={}
+  list=[]
+  for this in dict.keys():
+    #if this == 'lo':
+    #  continue
+    try:
+      thispref,indice,dummy = re.split('(\d+)',this)
+    except ValueError:
+      display.debug("{0} has no number skipping".format(this))
+      continue
+    if thispref != pref:
+      display.debug("{0} has not a prefix {1}".format(this,pref))
+      continue
+    # got issue with type casting 
+    if str(indice) == str(0) :
+      display.debug("Skipping indice {0}".format(this))
+      continue
+    addr=dict[this]['inet'][0]['address']
+    temp[indice]=addr
+  for i in sorted(temp.keys()):
+    display.debug('found primary iface #{0} add {1}'.format(i,temp[i]))
+    list.append(temp[i])
+  return(list)    
+
+
+
 def process_rs2if_grains(target,list,count,run):
   display.verbose("Getting all the grains from the server {0}".format(target))
   resp=local.cmd(target,'grains.items')
@@ -147,15 +181,20 @@ def main():
     display.error("Cant find target interface scality:prod_iface")
     exit(9)
   else:
-    display.debug("Found target interface : {0}".format(target))
+    display.debug("Found target interface : {0}".format(iface))
   network=get_network_settings(target)
   display.debug("Network settings {0}".format(network[target]))
   if not iface in network[target].keys():
     display.error("Cannot find interface {0} in network settings".format(iface))
   else:
     display.debug("interface {0} in network settings : {1}".format(iface,network[target].keys()))
-  secondary_list=get_secondary_sorted(network[target][iface],iface) 
-  process_rs2if_grains(target,secondary_list,int(args.count[0]),args.set)
+  if args.primary[0] != None:
+    primary=args.primary[0]
+    display.verbose("Using primary interface from {0}".format(primary))
+    if_list=get_primary_sorted(network[target],iface) 
+  else:
+    if_list=get_secondary_sorted(network[target][iface],iface) 
+  process_rs2if_grains(target,if_list,int(args.count[0]),args.set)
 
 if __name__ == '__main__':
   local = salt.client.LocalClient()
